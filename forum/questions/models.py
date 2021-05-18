757d4e51381cb4ca4fa0e33e4854b0f8211ed5b9
JELLO-Kim <cogus950403@gmail.com>
2021-05-18 13:00:38 +0900

    [Create] Question:models

--- forum/questions/models.py
+++ forum/questions/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+from users.models import User
+
+class Question(models.Model):
+    writter     = models.ForeignKey('users.User', on_delete=models.CASCADE)
+    title       = models.CharField(max_length=150)
+    content     = models.TextField()
+    created_at  = models.DateTimeField(auto_now_add=True)
+    updated_at  = models.DateTimeField(auto_now=True)
+    deleted_at  = models.DateTimeField(null=True)
+    is_delete   = models.BooleanField(default=0)
+
+    class Meta:
+        db_table = 'questions'
+
+class Comment(models.Model):
+    writter     = models.ForeignKey('users.User', on_delete=models.CASCADE)
+    question    = models.ForeignKey('Question', on_delete=models.CASCADE)
+    comment     = models.TextField()
+    is_parent   = models.ForeignKey('Comment', related_name='origin_comment', null=True, on_delete=models.CASCADE)
+    created_at  = models.DateTimeField(auto_now_add=True)
+    updated_at  = models.DateTimeField(auto_now=True)
+    deleted_at  = models.DateTimeField(null=True)
+    is_delete   = models.BooleanField(default=0)
+
+    class Meta:
+        db_table = 'comments'
+
+class QuestionLike(models.Model):
+    user = models.ForeignKey('users.User', on_delete=models.CASCADE)
+    question = models.ForeignKey('Question', on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'question_likes'
