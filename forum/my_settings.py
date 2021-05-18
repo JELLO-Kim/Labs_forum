5a22e1d8692160f585153d2cd9ee4f2326c41536
JELLO-Kim <cogus950403@gmail.com>
2021-05-18 12:09:53 +0900

    [Start] Start project

---
+++ forum/my_settings.py
@@ -0,0 +1,14 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'labs_forum',
+        'USER' : 'root',
+        'PASSWORD' : 'Toodifficult',
+        'HOST' : 'wecode-project.cpdsvq2c3ek9.ap-northeast-2.rds.amazonaws.com',
+        'PORT' : '3306'
+    }
+}
+
+SECRET_KEY = 'django-insecure-go1bp935l#f!oawaycfe!!&p9grw^_pv=hu^u3rtsfcq@co136'
+
+ALGORITHM = 'HS256'
