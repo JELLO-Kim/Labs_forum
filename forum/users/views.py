2856ac7f4a53c22771d6c5b4c85a5c82ffcb3db7
JELLO-Kim <cogus950403@gmail.com>
2021-05-18 19:31:06 +0900

    [Modify] url

--- forum/users/views.py
+++ forum/users/views.py
@@ -28,30 +28,29 @@ class SignUpView(View):
 
         # Key Validation
         if not email:
-            return JsonResponse({'message' '이메일을 입력해 주세요'}, status=400)
+            return JsonResponse({'message' : '이메일을 입력해 주세요'}, status=400)
         if not password:
-            return JsonResponse({'message' '비밀번호를 입력해 주세요'}, status=400)
+            return JsonResponse({'message' : '비밀번호를 입력해 주세요'}, status=400)
         if not name:
-            return JsonResponse({'message' '이름을 입력해 주세요'}, status=400)
+            return JsonResponse({'message' : '이름을 입력해 주세요'}, status=400)
         
         # 중복값 Validation
         if User.objects.filter(email=email).exists():
-            return JsonResponse({'message' '이미 사용중인 이메일입니다'}, status=400)
+            return JsonResponse({'message' : '이미 사용중인 이메일입니다'}, status=400)
         
         # password 형식 Validation
         if len(password) < 8:
             return JsonResponse({'message' : '비밀번호는 8자 이상 입력해주세요'}, status=400)
-        if password.regexp():
-            return JsonResponse({'message' : '특수문자는 @ ! ^ * 만 가능합니다.'}, status=400)
+        if password in "#":
+            return JsonResponse({'message' : '특수문자는 @ ! ^ * 만 가능합니다'}, status=400)
         
-        new_user = User.objects.create(
-                    email = email,
-                    password = password,
-                    name = name,
-                    user_type = 1
-                )
-
-        return JsonResponse({'message' : '회원가입 성공', 'result' : new_user.email}, status=201)
+        user = User.objects.create(
+                email = email,
+                password = password,
+                name = name,
+                user_type_id = 1
+            )
+        return JsonResponse({'message' : '회원가입 성공', 'result' : user.email}, status=201)
 
 class SignInView(View):
     def post(self, request):
@@ -73,18 +72,18 @@ class SignInView(View):
 
         # Key Validation
         if not email:
-            return JsonResponse({'message' '이메일을 입력해 주세요'}, status=400)
+            return JsonResponse({'message' : '이메일을 입력해 주세요'}, status=400)
         if not password:
-            return JsonResponse({'message' '비밀번호를 입력해 주세요'}, status=400)
+            return JsonResponse({'message' : '비밀번호를 입력해 주세요'}, status=400)
 
         # 유저정보 유효성 Validation
         if not User.objects.filter(email=email).exists():
-            return JsonResponse({'message' : '존재하지 않는 유저입니다.'}, status=400)
+            return JsonResponse({'message' : '존재하지 않는 유저입니다'}, status=400)
         else:
             if data['password'] != User.objects.get(email=email).password:
-                return JsonResponse({'message' : '비밀번호를 다시 확인해주세요'}, status=400)
+                return JsonResponse({'message' : '비밀번호가 잘못 입력되었습니다'}, status=400)
 
         user = User.objects.get(email=email)
-        access_token = jwt.encode({'id' : user.id}, SECRET_KEY, ALGORITHM=ALGORITHM)
+        access_token = jwt.encode({'id' : user.id}, SECRET_KEY, algorithm=ALGORITHM)
 
         return JsonResponse({'message' : '로그인 성공', 'access_token' : access_token}, status=200)        
