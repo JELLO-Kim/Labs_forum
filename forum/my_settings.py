020ae24df318af24291e64c585b3e65ea2dbd7f7
JELLO-Kim <cogus950403@gmail.com>
2021-05-18 19:43:14 +0900

    [Setting] 보안설정

--- forum/my_settings.py
+++ forum/my_settings.py
@@ -3,11 +3,24 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'labs_forum',
         'USER' : 'root',
-        'PASSWORD' : 'Toodifficult',
-        'HOST' : 'wecode-project.cpdsvq2c3ek9.ap-northeast-2.rds.amazonaws.com',
+        'PASSWORD' : 'Toohard',
+        'HOST' : '127.0.0.1',
         'PORT' : '3306'
     }
 }
+
+
+# RDS ver.
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'labs_forum',
+#         'USER' : 'root',
+#         'PASSWORD' : 'Toodifficult',
+#         'HOST' : 'wecode-project.cpdsvq2c3ek9.ap-northeast-2.rds.amazonaws.com',
+#         'PORT' : '3306'
+#     }
+# }
 
 SECRET_KEY = 'django-insecure-go1bp935l#f!oawaycfe!!&p9grw^_pv=hu^u3rtsfcq@co136'
 
