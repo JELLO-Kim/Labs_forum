cf430c154d263239a8a814a0126bf40a2ee12dbb
JELLO-Kim <cogus950403@gmail.com>
2021-05-18 13:01:15 +0900

    [Create] User:models

--- forum/users/models.py
+++ forum/users/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    email       = models.CharField(max_length=50, unique=True)
+    password    = models.CharField(max_length=300)
+    name        = models.CharField(max_length=30, unique=True, null=True)
+    user_type   = models.ForeignKey('UserType', on_delete=models.CASCADE)
+    last_login  = models.DateTimeField()
+    created_at  = models.DateTimeField(auto_now_add=True)
+    updated_at  = models.DateTimeField(auto_now=True)
+    deleted_at  = models.DateTimeField(null=True)
+    is_delete   = models.BooleanField(default=0)
+
+    class Meta:
+        db_table = 'users'
+
+class UserType(models.Model):
+    name = models.CharField(max_length=20)
+
+    class Meta:
+        db_table = 'user_types'
