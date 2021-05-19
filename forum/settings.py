0ea6f5ffc34ad25dbbd070118212d93cb1bf6a07
JELLO-Kim <cogus950403@gmail.com>
2021-05-19 20:01:57 +0900

    [Beta ver] - move all file's position - set docker - create docker-compose - create wait-for-it

--- forum/settings.py
+++ forum/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = SECRET_KEY
+SECRET_KEY = 'SECRET_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -75,7 +75,16 @@ WSGI_APPLICATION = 'forum.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = DATABASES
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'labs_forum_db',
+        'USER' : 'root',
+        'PASSWORD' : 'password',
+        'HOST' : '127.0.0.1',
+        'PORT' : '3306'
+    }
+}
 
 
 # Password validation
